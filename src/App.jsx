1c09118d7a77cb67975ba2ec31d226debce8bb7e
Dipendra Kumar <kdipendra5555@gmail.com>
2026-02-21 14:25:03 +0530

    Fix case-sensitive import for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpiPayment from "./UpiPayment";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import JoinOurMission from "./component/JoinOurMission";
 
 function App() {
